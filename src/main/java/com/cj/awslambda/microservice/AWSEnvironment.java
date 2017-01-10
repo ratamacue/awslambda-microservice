3cb3d57469bcdab73b2f32511a5fb1fe6050f6fb
David Ron <dron@cj.com>
2017-01-10 09:44:25 -0800

    Adding AWS Lambda Environment to the request.

---
+++ src/main/java/com/cj/awslambda/microservice/AWSEnvironment.java
@@ -0,0 +1,27 @@
+package com.cj.awslambda.microservice;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class AWSEnvironment {
+  private final Map<String, String> environment;
+  public AWSEnvironment(){
+    environment= System.getenv();
+  }
+  
+  /**
+   * Useful for testing.
+   * @param fakeEnvironment
+   */
+  public AWSEnvironment(Map<String, String> fakeEnvironment){
+    this.environment=fakeEnvironment;
+  }
+  
+  public Integer getMaximumAllocatedMemory(){return Optional.ofNullable(environment.get("AWS_LAMBDA_FUNCTION_MEMORY_SIZE")).map(Integer::parseInt).orElse(0);}
+  public String getAccessKeyId(){return environment.get("AWS_ACCESS_KEY_ID");}
+  public String getSecretAccessKey(){return environment.get("AWS_SECRET_ACCESS_KEY");}
+  public String getAccessKey(){return environment.get("AWS_ACCESS_KEY");}
+  public String getSessionToken(){return environment.get("AWS_SESSION_TOKEN");}
+  public String getLambdaFunctionName(){return environment.get("AWS_LAMBDA_FUNCTION_NAME");}
+  public String getRegion(){return environment.get("AWS_REGION");}
+}
--- src/main/java/com/cj/awslambda/microservice/Request.java
+++ src/main/java/com/cj/awslambda/microservice/Request.java
@@ -13,8 +13,9 @@ public class Request{
   public final String proxyPath;
   public final Optional<String> body;
   public final Context context;
+  public final AWSEnvironment awsEnvironment;
 
-  public Request(Map<String, Object> rawRequestFromAWS, Context contextFromAws){
+  public Request(Map<String, Object> rawRequestFromAWS, Context contextFromAws, AWSEnvironment awsEnvironment){
           //method =  Optional.ofNullable(RequestMethod.valueOf(rawRequestFromAWS.get("httpMethod")).toString()));
           method = Optional.ofNullable(rawRequestFromAWS.get("httpMethod")).map(Object::toString).map(RequestMethod::valueOf).orElse(RequestMethod.UNDEFINED);
           queryString = Optional.ofNullable(rawRequestFromAWS.get("queryStringParameters")).map(q->(Map<String, String>)q).orElse(new HashMap<>());
@@ -22,22 +23,24 @@ public class Request{
           proxyPath = Optional.ofNullable(rawRequestFromAWS.get("pathParameters")).map(x->(Map<String, String>)x).flatMap(m->Optional.ofNullable(m.get("proxy"))).orElse("/"); 
           body =  Optional.ofNullable(rawRequestFromAWS.get("body")).map(Object::toString);
           context=contextFromAws;
+          this.awsEnvironment=awsEnvironment;
   }
   
-  private Request(RequestMethod method, Map<String, String> queryString, String fullPath, String proxyPath, Optional<String> body, Context context){
+  private Request(RequestMethod method, Map<String, String> queryString, String fullPath, String proxyPath, Optional<String> body, Context context, AWSEnvironment awsEnvironment){
     this.method=method;
     this.queryString=queryString;
     this.fullPath=fullPath;
     this.proxyPath=proxyPath;
     this.body=body;
     this.context=context;
+    this.awsEnvironment = awsEnvironment;
     
   }
   
   public static class Builder{
     private final Request request; 
     public Builder(){
-      request = new Request(RequestMethod.GET, new HashMap<>(), "file://dev/null","", Optional.empty(), new FakeContext());
+      request = new Request(RequestMethod.GET, new HashMap<>(), "file://dev/null","", Optional.empty(), new FakeContext(), new AWSEnvironment(new HashMap<>()));
     }
     
     public Builder(Request request){
@@ -45,15 +48,15 @@ public class Request{
     }
     
     public Builder withMethod(RequestMethod method){
-      return new Builder(new Request(method, request.queryString, request.fullPath, request.proxyPath, request.body, request.context));
+      return new Builder(new Request(method, request.queryString, request.fullPath, request.proxyPath, request.body, request.context, request.awsEnvironment));
     }
     
     public Builder withProxyPath(String proxyPath){
-      return new Builder(new Request(request.method, request.queryString, request.fullPath, proxyPath, request.body, request.context));
+      return new Builder(new Request(request.method, request.queryString, request.fullPath, proxyPath, request.body, request.context, request.awsEnvironment));
     }
     
     public Builder withBody(Optional<String> body){
-      return new Builder(new Request(request.method, request.queryString, request.fullPath, request.proxyPath, body, request.context));
+      return new Builder(new Request(request.method, request.queryString, request.fullPath, request.proxyPath, body, request.context, request.awsEnvironment));
     }
     
     public Request build(){
